14c4e066dcf6aab3f5933c0fc43c7fcfe494199d
Eric Dietrich <eric_stefanon@msn.com>
2020-09-23 11:47:33 -0300

    login modal e tooltip created

---
+++ js/modules/scroll-animacao.js
@@ -0,0 +1,22 @@
+export default function initAnimacaoScroll() {
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
+    sections[0].classList.add('ativo');
+    const windowMetade = window.innerHeight * 0.6;
+
+    if (sections.length) {
+        function animaScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisibble = (sectionTop - windowMetade) < 0;
+                if (isSectionVisibble) {
+                    section.classList.add('ativo');
+                }
+                else {
+                    section.classList.remove('ativo');
+                }
+            });
+        }
+
+        window.addEventListener('scroll', animaScroll);
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,16 @@
+import initScrollSuave from './modules/scroll-suave.js';
+import initTabNav from './modules/tabnav.js';
+import initAccordion from './modules/accordion.js'
+import initAnimacaoScroll from './modules/scroll-animacao.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
+initScrollSuave();
+initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+
+
+
