885ddfc5cf949d779d9a3d377063d827cf198e2d
Eric Dietrich <eric_stefanon@msn.com>
2020-10-30 17:23:38 -0300

    numbers animation created

--- js/modules/scroll-animacao.js
+++ js/modules/scroll-animacao.js
@@ -11,7 +11,7 @@ export default function initAnimacaoScroll() {
                 if (isSectionVisibble) {
                     section.classList.add('ativo');
                 }
-                else {
+                else if (section.classList.contains('ativo')) {
                     section.classList.remove('ativo');
                 }
             });
